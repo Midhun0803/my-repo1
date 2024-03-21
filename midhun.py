83bdffabc7b6f39a8805a457190b390a54054f9c
Midhun0803 <164326202+Midhun0803@users.noreply.github.com>
2024-03-21 14:38:34 +0530

    Add files via upload

---
+++ midhun.py
@@ -0,0 +1,9 @@
+class person:
+  def __init__(self,name,age):
+    self.name=name
+    self.age=age
+
+P1=person('Rickon',25)
+print("P1=",P1.name,P1.age)
+P2=person('Raj',45)
+print("P2=",P2.name,P2.age)
